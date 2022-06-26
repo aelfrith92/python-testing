246bccba5ffe3da16b2713cbe84b4b924aacc754
Alfre <fred.morieri@gmail.com>
2022-06-26 22:15:27 +0000

    setUp() and tearDown() methods

--- test_evens.py
+++ test_evens.py
@@ -1,12 +1,36 @@
 import unittest
-from evens import even_number_of_evens
+from student import Student
 
 
-class TestEvens(unittest.TestCase):
+class TestStudent(unittest.TestCase):
     
-    def test_function_returns_True(self):
-        self.assertTrue(even_number_of_evens([]))
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
 
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
 if __name__ == "__main__":
     unittest.main()
